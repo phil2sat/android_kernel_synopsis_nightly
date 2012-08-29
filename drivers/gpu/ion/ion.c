5c358851b8c9177bb5ac9f510b024c8f814dd551
Rebecca Schultz Zavin <rebecca@android.com>
2012-08-29 10:30:08 -0700

    gpu: ion: Fix bug in ion_free
    Change-Id: I9e92ed750c5cd5d0329ce1747e419c908e0f37ef
    Signed-off-by: Rebecca Schultz Zavin <rebecca@android.com>

--- drivers/gpu/ion/ion.c
+++ drivers/gpu/ion/ion.c
@@ -509,6 +509,7 @@ void ion_free(struct ion_client *client, struct ion_handle *handle)
 	if (!valid_handle) {
 		mutex_unlock(&client->lock);
 		WARN(1, "%s: invalid handle passed to free.\n", __func__);
+		mutex_unlock(&client->lock);
 		return;
 	}
 	ion_handle_put(handle);
