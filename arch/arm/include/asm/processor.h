5b8d134d6577a2b289c879858324f1bbff0a4ec5
Sumit Singh <sumsingh@nvidia.com>
2014-02-18 18:36:28 +0530

    ARM: including asm-generic/processor.h
    Here we are including asm-generic/processor.h
    because it has defined some generic macros which
    can be used by any architecture for optimaztion
    of power usage.
    
    bug 1440421
    
    Change-Id: I39db944d348b3dad9235b76d8c7ea3867c0006ce
    Signed-off-by: Sumit Singh <sumsingh@nvidia.com>
    Reviewed-by: Alexander Van Brunt <avanbrunt@nvidia.com>
    Reviewed-by: Peng Du <pdu@nvidia.com>
    Reviewed-by: Sri Krishna Chowdary <schowdary@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- arch/arm/include/asm/processor.h
+++ arch/arm/include/asm/processor.h
@@ -3,6 +3,8 @@
  *
  *  Copyright (C) 1995-1999 Russell King
  *
+ *  Copyright (c) 2014, NVIDIA CORPORATION.  All rights reserved.
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
@@ -140,5 +142,7 @@ static inline void prefetchw(const void *ptr)
 #endif /* __LINUX_ARM_ARCH__ >= 5 */
 
 #endif /* __KERNEL__ */
+
+#include <asm-generic/processor.h>
 
 #endif /* __ASM_ARM_PROCESSOR_H */
