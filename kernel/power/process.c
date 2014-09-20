647b229ef6b0495d60d6a096486cd82c47897bc9
moddingg33k <moddingg33k@t-online.de>
2014-09-21 00:47:22 +0200

    Revert "PM: wakelock: Abort task freezing if a wake lock is held."
    This reverts commit 677f10402f85bfab6b540f6e3f64cca7f7cb79a4.
    
    Conflicts:
    	kernel/power/process.c

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -17,7 +17,7 @@
 #include <linux/delay.h>
 #include <linux/workqueue.h>
 #include <linux/kmod.h>
-#include <linux/wakelock.h>
+
 #include "power.h"
 
 /* 
@@ -71,10 +71,6 @@ static int try_to_freeze_tasks(bool user_only)
 			todo += wq_busy;
 		}
 
-		if (todo && has_wake_lock(WAKE_LOCK_SUSPEND)) {
-			wakeup = 1;
-			break;
-		}
 		if (!todo || time_after(jiffies, end_time))
 			break;
 
@@ -96,24 +92,12 @@ static int try_to_freeze_tasks(bool user_only)
 	elapsed_csecs = elapsed_csecs64;
 
 	if (todo) {
-		/* This does not unfreeze processes that are already frozen
-		 * (we have slightly ugly calling convention in that respect,
-		 * and caller must call thaw_processes() if something fails),
-		 * but it cleans up leftover PF_FREEZE requests.
-		 */
-		if(wakeup) {
-			printk("\n");
-			printk(KERN_ERR "Freezing of %s aborted\n",
-					user_only ? "user space " : "tasks ");
-		}
-		else {
-			printk("\n");
-			printk(KERN_ERR "Freezing of tasks %s after %d.%02d seconds "
-			       "(%d tasks refusing to freeze, wq_busy=%d):\n",
-			       wakeup ? "aborted" : "failed",
-			       elapsed_csecs / 100, elapsed_csecs % 100,
-			       todo - wq_busy, wq_busy);
-		}
+		printk("\n");
+		printk(KERN_ERR "Freezing of tasks %s after %d.%02d seconds "
+			"(%d tasks refusing to freeze, wq_busy=%d):\n",
+			wakeup ? "aborted" : "failed",
+			elapsed_csecs / 100, elapsed_csecs % 100,
+			todo - wq_busy, wq_busy);
 
 		if (!wakeup) {
 			read_lock(&tasklist_lock);
