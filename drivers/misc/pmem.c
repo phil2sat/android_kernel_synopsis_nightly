92fcdbdf2f49707ce7928e12f165af55886c02f8
moddingg33k <moddingg33k@t-online.de>
2014-09-20 19:22:20 +0200

    pmem: deprecate usage of 'pmem[id].reusable'
    This is no longer supported after removing fmem
    
    Signed-off-by: moddingg33k <moddingg33k@t-online.de>

--- drivers/misc/pmem.c
+++ drivers/misc/pmem.c
@@ -2902,7 +2902,7 @@ static int pmem_remove(struct platform_device *pdev)
 	pm_runtime_disable(&pdev->dev);
 	if (pmem[id].vbase)
 		iounmap(pmem[id].vbase);
-	if (pmem[id].map_on_demand && !pmem[id].reusable && pmem[id].area)
+	if (pmem[id].map_on_demand && pmem[id].area)
 		free_vm_area(pmem[id].area);
 	if (pmem[id].base)
 		free_contiguous_memory_by_paddr(pmem[id].base);
